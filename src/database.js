386c81da63d8ec570eb358ac1bde88ce9e89059f
amanda-santos <amandasf0200@gmail.com>
2023-01-27 19:20:53 -0300

    feat: 🗃️ create local database

---
+++ src/database.js
@@ -0,0 +1,42 @@
+import fs from "node:fs/promises";
+
+// import.meta.url is the current location of this file
+// this allows the database to be created always in the same location, instead of somewhere related to where the command is run
+const databasePath = new URL("../db.json", import.meta.url);
+
+export class Database {
+  // this makes the database a private property
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+
+    return data;
+  }
+}
